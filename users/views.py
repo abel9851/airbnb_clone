903b962c26f35ca1d1bbff3fdb1123074cdfeee7
Heejun Shin <abel9851@gmail.com>
2021-03-11 10:44:29 +0900

    Add SignUpView

--- users/views.py
+++ users/views.py
@@ -27,4 +27,20 @@ def logout_view(request):
 
 
 class SignUpView(FormView):
-    pass
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+    initial = {
+        "first_name": "HEEJUN",
+        "last_name": "SHIN",
+        "email": "abel9851@gmail.com",
+    }
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
