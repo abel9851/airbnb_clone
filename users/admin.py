578abc1f0ba2eed58ebef485381bf6682e517da6
Heejun Shin <abel9851@gmail.com>
2021-03-14 17:08:34 +0900

    Add Github Login

--- users/admin.py
+++ users/admin.py
@@ -27,6 +27,7 @@ class CustomUserAdmin(UserAdmin):
                     "language",
                     "currency",
                     "superhost",
+                    "login_method",
                 )
             },
         ),
@@ -45,4 +46,7 @@ class CustomUserAdmin(UserAdmin):
         "superhost",
         "is_staff",
         "is_superuser",
+        "email_verified",
+        "email_secret",
+        "login_method",
     )
